0c30d099ed3c8db2ce35421664430b1ab8aa433b
Deepak Rajoriya <deepak874205@gmail.com>
2023-10-10 17:54:42 +0530

    update header

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const selectHeader = document.querySelector('#header');
   if (selectHeader) {
     document.addEventListener('scroll', () => {
-      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+      window.scrollY > 400 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
     });
   }
 
